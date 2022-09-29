8476a3fb5b453cc022c4a6a17efdfd7a15f6ab60
hopefyjamshed <103117529+hopefyjamshed@users.noreply.github.com>
2022-09-29 23:34:21 +0600

    sidemenu added

--- src/Component/Sidemenu/Sidemenu.js
+++ src/Component/Sidemenu/Sidemenu.js
@@ -1,3 +1,7 @@
+
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import React from 'react';
 import jamsh from '../../jamsh.jpg'
 import './Sidemenu.css'
@@ -7,7 +11,28 @@ import './Sidemenu.css'
 const Sidemenu = () => {
     return (
         <div>
-            <img className='jamsh' src={jamsh} alt="" />
+            <div className='personal-info'>
+                <img className='jamsh' src={jamsh} alt="" />
+                <div>
+                    <h3 className='my-name'>Towhidul Islam</h3>
+                    <p className='location'><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> <span>chittagong, Bangladesh</span></p>
+                </div>
+            </div>
+            <div className='personal-data'>
+                <div className="data">
+                    <h2 className='h2'>65 <span className='small'>kg</span> </h2>
+                    <p className='quantity'>weigth</p>
+                </div>
+                <div className="data">
+                    <h2 className='h2'>5.11 <span className='small'>inch</span> </h2>
+                    <p className='quantity'>Height</p>
+                </div>
+                <div className="data">
+                    <h2 className='h2'>26 <span className='small'>Years</span> </h2>
+                    <p className='quantity'>Age</p>
+                </div>
+
+            </div>
         </div>
     );
 };
